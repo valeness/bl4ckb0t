47e0dda388eb9112f0cf03395bfd42152209c641
bl4ckw4ter <derp@derp>
2015-11-30 15:43:58 -0500

    adding source

---
+++ modules/source.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+source.py - where ur source
+"""
+
+def source(jenni, input):
+    jenni.say('https://github.com/bl4ckw4ter/bl4ckb0t.git')
+source.commands = ['source']
+source.priority = 'low'
+
+if __name__ == '__main__':
+    print __doc__.strip()
